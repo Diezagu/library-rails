2160bdc10a96717f6042f3dec0d49edea81cec16
Diego Zaizar <diegozaizar26@gmail.com>
2022-05-18 11:59:53 -0500

    Add not null validation at db level

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 2022_05_18_033822) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
+    t.string "name", null: false
+    t.integer "age", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
