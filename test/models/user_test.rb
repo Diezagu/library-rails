7a73748e63272dc846fe1cd07c8ca3856d71d072
Diego Zaizar <diegozaizar26@gmail.com>
2022-05-23 15:06:52 -0500

    Modify users fixtures and correct model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,8 +3,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  test 'it is not valid without name' do
+    user = User.new(age: 24, email: 'user@example.com')
+
+    refute user.valid?
+    assert_equal user.errors.messages[:name].first, "can't be blank"
+  end
+
+  test 'it is not valid without age' do
+    user = User.new(name: 'Diego')
+
+    refute user.valid?
+    assert_equal user.errors.messages[:age].first, "can't be blank"
+  end
+
   test 'it is not valid if age is greater than 100' do
-    user = User.new(name: 'Diego', age: 101, email: 'user@exmaple.com')
+    user = User.new(name: 'Diego', age: 101)
 
     refute user.valid?
     assert_equal user.errors.messages[:age].first, 'must be less than 100'
@@ -15,11 +29,5 @@ class UserTest < ActiveSupport::TestCase
 
     refute user.valid?
     assert_equal user.errors.messages[:age].first, 'must be greater than 0'
-  end
-
-  test 'it is valid with name and age' do
-    user = User.new(name: 'Diego', age: 24)
-
-    assert user.valid?
   end
 end
