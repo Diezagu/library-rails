52f0864e53dedd5b9b166bc2655f6003de32a2c4
Diego Zaizar <diegozaizar26@gmail.com>
2022-05-23 14:58:30 -0500

    Correct all rubocop offenses

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,22 +1,10 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test 'it is not valid without name' do
-    user = User.new(age: 24)
-
-    refute user.valid?
-    assert_equal user.errors.messages[:name].first, "can't be blank"
-  end
-
-  test 'it is not valid without age' do
-    user = User.new(name: 'Diego')
-
-    refute user.valid?
-    assert_equal user.errors.messages[:age].first, "can't be blank"
-  end
-
   test 'it is not valid if age is greater than 100' do
-    user = User.new(name: 'Diego', age: 101)
+    user = User.new(name: 'Diego', age: 101, email: 'user@exmaple.com')
 
     refute user.valid?
     assert_equal user.errors.messages[:age].first, 'must be less than 100'
