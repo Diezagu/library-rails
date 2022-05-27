846431a7627dc9d98513a4ed998462d9a7128f6f
Diego Zaizar <diegozaizar26@gmail.com>
2022-05-27 11:49:33 -0500

    Create controller actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,22 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'must have index' do
+    get users_path
+
+    assert_response :success
+  end
+
+  test 'should show a user' do
+    get user_path(users(:diego))
+
+    assert_response :success
+  end
+
+  test 'should delete a user' do
+    delete user_path(users(:diego))
+
+    assert_equal flash[:notice], 'User deleted!'
+    assert_redirected_to users_path
+  end
 end
