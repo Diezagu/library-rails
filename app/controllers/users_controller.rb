796fc4773e2e518a316f0c3b5acdc006efaca6f3
Diego Zaizar <diegozaizar26@gmail.com>
2022-05-27 11:49:33 -0500

    Create controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      flash[:notice] = 'User deleted!'
+    else
+      flash[:alert] = 'Error while deleting user!'
+    end
+    redirect_to users_path
+  end
 end
