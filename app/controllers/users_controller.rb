bde93c79fb3520516927e14cc9ac70d08750e9a0
Diego Zaizar <diegozaizar26@gmail.com>
2022-05-27 15:03:49 -0500

    Use before_action instead repeating lines

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,25 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :find_user, only: %i[show destroy]
   def index
     @users = User.all
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
   def destroy
-    @user = User.find(params[:id])
     if @user.destroy
       flash[:notice] = 'User deleted!'
     else
       flash[:alert] = 'Error while deleting user!'
     end
     redirect_to users_path
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
