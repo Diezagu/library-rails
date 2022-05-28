bd2168ad95423d6f7eed877d8c61e692d0aa6028
Diego Zaizar <diegozaizar26@gmail.com>
2022-05-28 18:50:34 -0500

    Create users views and set root page to users_path

---
+++ test/system/users_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class UsersTest < ApplicationSystemTestCase
+  test 'visiting index' do
+    visit users_path
+
+    assert_content 'Authors'
+  end
+
+  test 'visiting the show of user' do
+    visit users_path
+    click_link 'Show'
+
+    assert_content users(:diego).name
+  end
+
+  test 'deleting a user' do
+    visit users_path
+    click_link 'Destroy'
+
+    assert_content 'User deleted!'
+  end
+end
