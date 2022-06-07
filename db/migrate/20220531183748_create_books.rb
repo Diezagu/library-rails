8ae6b3b5a26233219b95a98ef1e79db9a988ef13
Diego Zaizar <diegozaizar26@gmail.com>
2022-06-06 23:40:56 -0500

    Correct name of pages variable, use delegate and add foreign key

--- db/migrate/20220531183748_create_books.rb
+++ db/migrate/20220531183748_create_books.rb
@@ -4,10 +4,11 @@ class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
       t.string :title, null: false
-      t.integer :pages, null: false
+      t.integer :number_of_pages, null: false
       t.belongs_to :author
 
       t.timestamps
     end
+    add_foreign_key :books, :users, column: :author_id
   end
 end
