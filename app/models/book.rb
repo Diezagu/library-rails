31ebd72662bd455bd8749c6ac1e7c5dea19e3280
Diego Zaizar <diegozaizar26@gmail.com>
2022-06-13 12:39:02 -0500

    Add book cover

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,9 @@
 
 class Book < ApplicationRecord
   validates :title, :number_of_pages, presence: true
+  validates :cover, content_type: ['image/png', 'image/jpg', 'image/jpeg']
   belongs_to :author, class_name: 'User'
 
   delegate :name, to: :author, prefix: 'author'
+  has_one_attached :cover
 end
