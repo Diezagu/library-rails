6f32139c4de78a75e5bf7d9be8324260bf4fa1fd
Diego Zaizar <diegozaizar26@gmail.com>
2022-07-03 21:19:56 -0500

    Create likes controller

--- db/migrate/20220704004435_create_likes.rb
+++ db/migrate/20220704004435_create_likes.rb
@@ -3,9 +3,13 @@ class CreateLikes < ActiveRecord::Migration[6.0]
     create_table :likes do |t|
       t.belongs_to :author
 <<<<<<< HEAD
+<<<<<<< HEAD
       t.belongs_to :book
 =======
 >>>>>>> 6e98d92... Create like model
+=======
+      t.belongs_to :book
+>>>>>>> b1a132f... Create likes controller
       t.timestamps
     end
   end
