596f4771083bd84e5fb58e74a64b03f5a5b6620e
Diego Zaizar <diegozaizar26@gmail.com>
2022-07-04 12:41:15 -0500

    Create likes controller and add correct route

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,24 @@
+class LikesController < ApplicationController
+  def create
+    @like = Like.new(permitted_params)
+    if !@like.save
+      flash[:alert] = 'Error while liking book!'
+    end
+    redirect_to user_path(@like.
+    )
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+    if !@like.destroy
+      flash.now[:alert] = 'Error while unliking book!'
+    end
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def permitted_params
+    params.require(:like).permit(:author_id, :book_id)
+  end
+end
