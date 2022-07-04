91b5c272efd769962b9cf322858ccbe96b3ac073
Diego Zaizar <diegozaizar26@gmail.com>
2022-07-04 12:42:08 -0500

    Add foreign keys and unique validation

--- db/migrate/20220704004435_create_likes.rb
+++ db/migrate/20220704004435_create_likes.rb
@@ -5,5 +5,8 @@ class CreateLikes < ActiveRecord::Migration[6.0]
       t.belongs_to :book
       t.timestamps
     end
+    add_foreign_key :likes, :books, column: :book_id
+    add_foreign_key :likes, :users, column: :author_id
+    add_index :likes, [:author_id, :book_id], unique: true
   end
 end
