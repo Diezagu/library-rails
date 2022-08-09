bfaf93f1c488c51bbe3592e0522aac7a58da14c4
Diego Zaizar <diegozaizar26@gmail.com>
2022-08-09 00:16:40 -0500

    Correct issues of naming and  not null validations

--- db/migrate/20220802204812_create_notifications.rb
+++ db/migrate/20220802204812_create_notifications.rb
@@ -1,8 +1,8 @@
 class CreateNotifications < ActiveRecord::Migration[6.0]
   def change
     create_table :notifications do |t|
-      t.string :title
-      t.text :text
+      t.string :title, null: false
+      t.text :text, null: false
       t.belongs_to :author, foreign_key: { to_table: :user }
 
       t.timestamps
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -4,17 +4,13 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test 'notification' do
-    # Create the email and store it for further assertions
     email = UserMailer.with(user: users(:urbi),
                             current_user: users(:diego),
                             book: books(:first)).notification_email
 
-    # Send the email, then test that it got queued
     assert_emails 1 do
       email.deliver_now
     end
-
-    # Test the body of the sent email contains what we expect it to
     assert_equal ['notifications@library.com'], email.from
     assert_equal ['user2@example.com'], email.to
     assert_equal 'New book created!', email.subject
