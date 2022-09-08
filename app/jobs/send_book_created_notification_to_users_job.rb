8b63e514c1d89ead26d9d8dffc94d1eb80aafe26
“Diego <diegozaizar26@gmail.com>
2022-09-08 00:42:05 -0500

    Solve all suggestions

--- app/jobs/send_book_created_notification_to_users_job.rb
+++ app/jobs/send_book_created_notification_to_users_job.rb
@@ -4,13 +4,11 @@ class SendBookCreatedNotificationToUsersJob < ApplicationJob
   def perform(author_id, book_id)
     followers = Follow.where(followee_id: author_id)
     author = User.find_by(id: author_id)
-    book = Book.find_by(id: book_id)
     followers.each do |follower|
-      user = User.find_by(id: follower.follower_id)
-      UserMailer.with(user: user, current_user: author, book: book).notification_email.deliver_now
+      UserMailer.with(follower_id: follower.id, author_id: author_id, book_id: book_id).notification_email.deliver_now
       Notification.create(title: 'New book created',
                           text: "#{author.name} has published a new book",
-                          author_id: follower.follower_id)
+                          author_id: follower.id)
     end
   end
 end
