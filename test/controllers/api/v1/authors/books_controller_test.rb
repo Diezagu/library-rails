2f7b41fb5a33723fbf82b5ab3f72b00c2d2e26c0
“Diego <diegozaizar26@gmail.com>
2022-09-14 20:23:16 -0500

    Add tests to API books controller and solve rubocop issues

---
+++ test/controllers/api/v1/authors/books_controller_test.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Api::V1::BooksControllerTest < ActionDispatch::IntegrationTest
+  test 'should get books of an specific author' do
+    book = {
+      id: books(:first).id,
+      title: books(:first).title,
+      number_of_pages: books(:first).number_of_pages,
+      author_id: books(:first).author_id,
+      created_at: books(:first).created_at.iso8601(3),
+      updated_at: books(:first).updated_at.iso8601(3)
+    }
+
+    get api_v1_author_books_path(users(:diego))
+
+    assert_response :success
+    assert_equal [book.as_json], JSON.parse(response.body)
+  end
+end
