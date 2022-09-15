98145e12d7760699cb075456966f3f0079930005
“Diego <diegozaizar26@gmail.com>
2022-09-14 19:00:13 -0500

    Modify controller to simplify request

--- app/controllers/api/v1/authors/books_controller.rb
+++ app/controllers/api/v1/authors/books_controller.rb
@@ -4,12 +4,17 @@ module Api
   module V1
     module Authors
       class BooksController < ApplicationController
+        before_action :setup_author
+
         def index
-          if User.find_by(id: params[:author_id])
-            render json: User.find(params[:author_id]).books
-          else
-            render json: {}, status: :not_found
-          end
+          render json: @author.books
+        end
+
+        private
+
+        def setup_author
+          @author = User.find_by(id: params[:author_id])
+          render json: {}, status: :not_found unless @author
         end
       end
     end
